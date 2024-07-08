9a4a3b2e1b661c3e1b87755413c6417eae43c572
Rashidprojects <rashidgitproject@gmail.com>
2024-07-08 09:10:32 +0530

    updated slug of products and product details

---
+++ Frontpage/views.py
@@ -0,0 +1,162 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from Core.models import Banner,Gallery_Image,Review,Enquiry,Partners,Event,Schools,Premium,Single,Double
+from Frontpage.models import Visitor
+import uuid
+from django.contrib import messages
+
+# Create your views here.
+
+def update_visitors():
+    key = str(uuid.uuid4().hex)[:8]
+    Visitor.objects.create(Key=key)
+
+
+def home(request):
+    mobile_banners = Banner.objects.filter(Banner_Type='Mobile').order_by('-id')
+    system_banners = Banner.objects.filter(Banner_Type='System').order_by('-id')
+    images = Gallery_Image.objects.all().order_by('-id')[:6]
+    reviews = Review.objects.all().order_by('-id')[:6]
+    partners = Partners.objects.all().order_by('-id')
+    events = Event.objects.all().order_by('-Date')[:3]
+    schools = Schools.objects.all().order_by('-id')[:2]
+
+    update_visitors()
+
+    context = {
+        'mobile_banners' : mobile_banners,
+        'system_banners' : system_banners,
+        'images' : images,
+        'reviews' : reviews,
+        'partners' : partners,
+        'events' : events,
+        'schools' : schools,
+    }
+    return render(request,'Frontpage/index.html',context)
+
+def index(request):
+    return render(request, 'Frontpage/indexpetra.html')
+
+def about(request):
+    reviews = Review.objects.all().order_by('-id')
+    partners = Partners.objects.all().order_by('-id')
+
+    context = {
+        'reviews' : reviews,
+        'partners' : partners
+    }
+    return render(request,'Frontpage/about.html',context)
+
+def products(request):
+    premiums = Premium.objects.all()
+    singles = Single.objects.all().order_by('-id')
+    doubles = Double.objects.all().order_by('-id')
+    
+    print(premiums)
+    print(singles)
+    print(doubles)
+    
+    context = {
+        'premium' : premiums,
+        'single' : singles,
+        'double' : doubles
+    }
+    return render(request, 'Frontpage/products.html',context)
+
+
+def product_detail(request, category, slug):
+    if category == 'premium':
+        product = get_object_or_404(Premium, slug=slug)
+        suggest = Premium.objects.exclude(slug=slug)
+    elif category == 'single':
+        product = get_object_or_404(Single, slug=slug)
+        suggest = Single.objects.exclude(slug=slug)
+    elif category == 'double':
+        product = get_object_or_404(Double, slug=slug)
+        suggest = Double.objects.exclude(slug=slug)
+    else:
+        product = None
+        suggest = None
+
+    print("Product:", product)
+    print("Suggestions:", suggest)
+
+    return render(request, 'Frontpage/product_detail.html', {'product': product, 'suggest': suggest, 'category': category})
+
+def findDealers(request):
+    return render(request, 'Frontpage/findNearDealers.html')
+
+def becomeDealer(request):
+    return render(request, 'Frontpage/becomeAdealers.html')
+
+def registerComplaint(request):
+    return render(request, 'Frontpage/registerComplaint.html')
+
+def registerwarranty(request):
+    return render(request, 'Frontpage/registerwarranty.html')
+
+def testimonials(request):
+    return render(request, 'Frontpage/testimonials.html')
+
+def blogs(request):
+    return render(request, 'Frontpage/blogs.html')
+
+@csrf_exempt
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        mobile = request.POST.get('mobile')
+        description = request.POST.get('description')
+
+        try:
+            Enquiry.objects.create(Name=name,Mobile=mobile,Description=description)
+            return JsonResponse({'status':'success'})
+        except:
+            return JsonResponse({'status':'failed'})
+    return render(request,'Frontpage/contact.html')
+
+def events(request):
+    events = Event.objects.all().order_by('-Date')
+
+    context = {
+        'events' : events
+    }
+    return render(request,'Frontpage/events.html',context)
+
+def gallery(request):
+    images = Gallery_Image.objects.all().order_by('-id')
+
+    context = {
+        'images' : images
+    }
+    return render(request,'Frontpage/gallery.html',context)
+
+def packages(request):
+    schools = Schools.objects.all().order_by('-id')
+
+    context = {
+        'schools' : schools
+    }
+    return render(request,'Frontpage/packages.html',context)
+
+def rides(request):
+    return render(request,'Frontpage/rides.html')
+
+@csrf_exempt
+def review(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        place = request.POST.get('place')
+        description = request.POST.get('description')
+        rating = request.POST.get('rating')
+
+        try:
+            Review.objects.create(Name=name,Place=place,Description=description,Rating=rating)
+            # return JsonResponse({'status':'success'})
+            messages.success(request,'Review Added Successfully ... !')
+            return redirect('review')
+        except:
+            return JsonResponse({'status':'failed'})
+        
+    return render(request,'Frontpage/review.html')
